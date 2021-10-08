926e4ec02e282adadb046b64d4c75f896e58715c
kritie <kritiegupta250@gmail.com>
2021-10-08 23:05:13 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-ogkt4n
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-ogkt4n)
---
+++ index.js
@@ -0,0 +1,16 @@
+// Import stylesheets
+import './style.css';
+
+// // Write Javascript code!
+// const appDiv = document.getElementById('app');
+// appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+// // var b;
+// // console.log('this is b', b);
+// // var c = null;
+// // console.log('this is c', c);
+// // console.log(f);
+
+// var arr = [1, 3, 5, 8, 2];
+// var arrB = arr.reverse();
+// console.log(arrB);
